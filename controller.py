b6467c31acdba14d8dbb2e791998bc27491dc425
nithishkumarS <nithishtimes@gmail.com>
2020-01-13 21:37:19 -0500

    Added socket transfer modules

--- controller.py
+++ controller.py
@@ -1,52 +1,77 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import json
 import sys
+import socket
 from keywords import *
 
+if len(sys.argv) != 3:
+    print("usage: ./controller.py <test> <port>")
+    exit(1)
+
 testFile = sys.argv[1]
+PORT = int(sys.argv[2])
 
 
 def read_test():
-    f = open( testFile, 'r')
-    test = json.loads( f.read())
+    f = open(testFile, 'r')
+    test = json.loads(f.read())
     f.close()
     return test
 
 
-
- 
 class controller():
-    def __init__(self):
-        self.text_input = read_test()
-        self.parse_input()
-    
+    def __init__(self, input=1):
+        self.text_input = input
+        # self.parse_input()
+        # self.set_initial_states()
+
+    def get_input(self, _input):
+        self.text_input = _input
+
     def parse_input(self):
         self.arguements = self.text_input[keys[0]]
         self.programs = self.text_input[keys[1]]
         self.configuration = self.text_input[keys[2]]
 
-	def set_initial_config(self):
-		print('initial config')
+    def num_of_test_cases(self):
+        return len(self.programs)
 
-    # def generate_ouput(self):
+    def set_initial_states(self):
+        self.sensors = {}
+        self.devices = {}
+        # print(self.configuration['sensors']['owner_location']\)
+        for key in self.configuration['sensors'].keys():
+            print('key:', key)
+            self.sensors[key] = int(self.configuration['sensors'][key])
 
-    #   for prog in progs:
-       
-    #       results = "["
-    #       oneline=False
-    #       for line in readlines(s):
-    #           print(line)
-    #           if (oneline):
-    #               results += ", "
-    #           results += line
-    #           oneline = True
-    #       results += "]"
-
+        for key in self.configuration['output_devices'].keys():
+            self.devices[key] = int(self.configuration['output_devices'][key])
 
 def main():
-	obj = controller()
+    host_ip = "localhost"
+    wait = True
+    obj = controller()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host_ip, PORT))
+        s.listen()
+        print('listening')
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                _input = conn.recv(4096)
+                if _input:
+                    obj.get_input(_input)
+                    print(_input)
+                    print('received')
+                    output = obj.generate_ouput().encode()
+                conn.sendall(output)
+
+        print('connection terminated')
+    print('end')
 
 
 if __name__ == "__main__":
-	main()
+    main()
